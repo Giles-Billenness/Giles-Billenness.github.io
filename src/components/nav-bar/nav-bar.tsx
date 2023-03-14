c372a8b72225862777db3f724ff6bdb06f86ba47
Giles Billenness <giles.billenness@blueyonder.co.uk>
2023-03-14 21:53:21 +0000

    Remove old code

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -20,33 +20,7 @@ export interface NavBarProps {
  * For details on on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-nav-bars-and-templates
  */
 export const NavBar = ({ className, children = 'NavBar' }: NavBarProps) => {
-    // const navRef = useRef<HTMLInputElement>(null);
-    // const showNavBar = () => {
-    //     if (navRef.current) {
-    //         navRef.current.classList.toggle('responsive_nav');
-    //     }
-    // };
-
     return (
-        // <div className={classNames(styles.root, className)}>
-        //     <header>
-        //         <h3>
-        //             <Logo className="logo"/>
-        //             <nav className="nav-links" ref={navRef}>
-        //                 <a href="/#about">./ About</a>
-        //                 <a href="/#experience">./ Experience</a>
-        //                 <a href="/#projects">./ Projects</a>
-        //                 <a href="/#contact">./ Contact</a>
-        //                 <button className="nav-btn nav-close-btn" onClick={showNavBar}>
-        //                     <FaTimes />
-        //                 </button>
-        //             </nav>
-        //             <button className="nav-btn" onClick={showNavBar}>
-        //                 <FaBars />
-        //             </button>
-        //         </h3>
-        //     </header>
-        // </div>
         <>
             <Navbar sticky="top" key='md' expand='md' className={styles.Navbar}>
             <Container fluid className={styles.Container}>
